c69a39c5330c5c9be9453aab7e9cc7b40ec98487
eunggi-Domfam <eunggi3374@gmail.com>
2020-12-03 17:29:12 +0900

    모달 잡는 중 화면 움직이는 이슈

---
+++ src/components/popup/Alert.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+ <slot :toggleAlert="toggleAlert"></slot>
+    <b-modal centered modal-class="normalPopup" v-model="showAlert">
+      <template #modal-header>
+        <div class="symbol"><img src="@/assets/images/common/drawing@2x.png" alt=""></div>
+      </template>
+      <p class="text text-md">학습 주제 영역입니다.<br/>
+        학습 주제는 최대 세 문장까지<br/>
+        가능합니다.</p>
+      <p class="text-sm">생각 제시하는 생각 과제 제시하는 텍스트<br/>
+        영역으로 최대 두줄 이상을 생각합니다.</p>
+      <template #modal-footer="{ cancel }">
+        <b-button class="btn btn-block btn-black" @click="cancel()">닫기</b-button>
+      </template>
+    </b-modal>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Alert',
+  data(){
+    return{
+      showAlert : false,
+      type : '',
+      text :{
+
+      }
+    }
+  },
+  methods : {
+    toggleAlert(type){
+      this.showAlert = !this.showAlert;
+      this.type = type
+    }
+  }
+}
+</script>
+
+
---
+++ src/components/popup/Confirm.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    <slot :toggleConfirm="toggleConfirm"></slot>
+    <b-modal centered title="마케팅 관련 정보 수신 동의" modal-class="normalPopup" v-model="showConfirm">
+      <template #modal-header>
+        <div class="symbol">
+          <img src="@/assets/images/common/check_red@2x.png" alt="">
+        </div>
+      </template>
+      <p class="text">뒤로 가시겠어요?<br/></p>
+      <p v-if="type==='goToBack2'" class="text-sm">영상이 아직 끝나지 않았습니다.</p>
+      <template v-if="type==='goToback'" #modal-footer="{ cancel }">
+        <b-button variant="gray" class="btn-half" @click="cancel()">아니요</b-button>
+        <router-link to="/PabloStudy1" class="btn btn-black btn-half">네</router-link>
+      </template>
+
+      <template v-if="type==='goToBack2'" #modal-footer="{ cancel }">
+        <router-link to="PabloStudy2" class="btn btn-gray btn-half">뒤로 갈래요</router-link>
+        <b-button variant="black" class="btn-half" @click="cancel()">닫기</b-button>
+      </template>
+    </b-modal>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Confirm',
+  data(){
+    return{
+      showConfirm : false,
+      type : "",
+      text : {
+
+      },
+      src : {
+        check_red : "@/assets/images/common/check_red@2x.png"
+      }
+    }
+  },
+  methods:{
+    toggleConfirm(type){
+      this.showConfirm  = !this.showConfirm;
+      this.type = type;
+    }
+  }
+
+}
+</script>
+
+<style scoped>
+
+</style>
