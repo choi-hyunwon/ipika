46472153cc9763404e2ee9328e67880a227f98e7
choi hyunwon <danboys@domfam.com>
2020-12-15 22:36:52 +0900

    사용자 아이디 노출

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,7 @@
 <template>
   <div id="app">
+    <div class="userid">사용자 ID : {{ $store.state.session.user_id }}</div>
+
     <router-view/>
   </div>
 </template>
@@ -21,4 +23,19 @@ export default {
 </style>
 
 <style>
+#app {
+  width: 1280px;
+  position: relative;
+  margin: 0 auto;
+}
+
+.userid {
+  position: absolute;
+  right: 0;
+  bottom: 0;
+  background-color: #fff;
+  border: 1px solid #000;
+  padding: 5px;
+  z-index: 1000;
+}
 </style>
