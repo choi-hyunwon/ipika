90b6ab43bbad0c336633af728c9399d0748bdb9e
choi hyunwon <danboys@domfam.com>
2020-12-15 22:37:19 +0900

    vue.config.js 삭제

--- vue.config.js
+++ vue.config.js
@@ -3,8 +3,8 @@ const path = require('path');
 module.exports = {
   lintOnSave: false,
   // outputDir : path.resolve(__dirname, './web')
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/web/'
-    : '/'
+  // publicPath: process.env.NODE_ENV === 'production'
+  //   ? '/web/'
+  //   : '/'
 }
 
