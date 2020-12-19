bf189797242f0e4c6a253dd4c66f42170d66923f
최현원 <danboys@domfam.com>
2020-12-19 21:15:57 +0900

    [파블로레터] 학습완료 > 학습 완료 이미지 어드민 등록된 이미지와 다름

--- src/views/letter/Completion.vue
+++ src/views/letter/Completion.vue
@@ -13,7 +13,7 @@
         </div>
       </div>
       <div class="col col-6">
-        <div class="img"><img src="@/assets/images/temp/sample_img_01.jpg" alt=""></div>
+        <div class="img"><img :src="letter.stepCompleteImageUrl" alt=""></div>
       </div>
     </div>
   </div>
