34d03cf8dbde76824dec70dba343989fc292cd1d
최현원 <danboys@domfam.com>
2020-12-22 20:34:57 +0900

    0010584: [파블로 아트] Free drawing 완료 선택 시 제목 입력 팝업 내 닫기,등록하기 버튼이 활성화로 노출되지 않음

--- src/components/popup/Register.vue
+++ src/components/popup/Register.vue
@@ -37,7 +37,7 @@
         <input type="text" class="form-control active" id="text" v-model="title" @input="checkInputSize" @keydown.enter="registerFree" placeholder="여기에 입력해주세요">
       </form>
       <template #modal-footer="{ ok }">
-        <button class="btn submit_button" variant="btn black btn-block" @click="cancel">닫기</button>
+        <button class="btn submit_button" variant="btn black btn-block" :class="{ 'btn-black' : title == '' }" @click="cancel">닫기</button>
         <button class="btn submit_button" variant="btn black btn-block" :class="{ 'btn-blue5' : !title == '' }" @click="registerFree">등록하기</button>
       </template>
     </b-modal>
