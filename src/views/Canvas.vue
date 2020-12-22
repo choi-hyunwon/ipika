62a6299059a9b8aa2e3617d825746c72a2982514
최현원 <danboys@domfam.com>
2020-12-22 20:33:45 +0900

    흰배경처리 레터만 제외

--- src/views/Canvas.vue
+++ src/views/Canvas.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="wrap" >
+  <div class="wrap">
     <!--   canvas 헤더 -->
     <CanvasHeader v-if="isLoading"></CanvasHeader>
     <!--   wacom 라이브러리 -->
@@ -22,17 +22,18 @@
     <Confirm v-if="page==='diagnose'" ref="timerConfirm"/>
 
     <!--   진단테스트 드로잉 제출 완료 시 popup-->
-    <Alert ref="autoOpenSuccess" v-slot="slotProps" :boldText="'성공적으로 </br> 제출되었어요 :)'" :text="'결과를 확인해보세요'" :buttonText ="'내 스테이지 확인하러 가기'"/>
+    <Alert ref="autoOpenSuccess" v-slot="slotProps" :boldText="'성공적으로 </br> 제출되었어요 :)'" :text="'결과를 확인해보세요'"
+           :buttonText="'내 스테이지 확인하러 가기'"/>
 
     <!--   학습 드로잉 제출 완료 시 popup-->
     <Confirm ref="autoOpenLSuccess" v-slot="slotProps" :completeText="`${bg.imageName} 저장되었어요<br> 남은 그림도 더 그려볼까요?`"
-             :text="'모든 배경교재를 그려야 학습과정이 완료돼요'" :cancelText = "'아니요'" :okText = "'네'"/>
+             :text="'모든 배경교재를 그려야 학습과정이 완료돼요'" :cancelText="'아니요'" :okText="'네'"/>
 
     <!-- 공통 알림 popup-->
-    <Alert ref="commonAlert" v-slot="slotProps" :boldText="'그림이 그려지지 않았어요'" :text="'그림을 시작해 볼까요?'" :buttonText ="'확인'"/>
+    <Alert ref="commonAlert" v-slot="slotProps" :boldText="'그림이 그려지지 않았어요'" :text="'그림을 시작해 볼까요?'" :buttonText="'확인'"/>
 
     <!-- 공통 알림 popup-->
-    <Alert ref="submissionFail" v-slot="slotProps" :boldText="'드로잉 제출 실패하였습니다'" :text="'앗! 이런'" :buttonText ="'확인'"/>
+    <Alert ref="submissionFail" v-slot="slotProps" :boldText="'드로잉 제출 실패하였습니다'" :text="'앗! 이런'" :buttonText="'확인'"/>
 
 
   </div>
@@ -49,7 +50,7 @@ import store from '@/store/store'
 
 export default {
   name: 'Canvas',
-  components :{
+  components: {
     Alert,
     Wacom,
     Confirm,
@@ -59,69 +60,78 @@ export default {
   data () {
     return {
       isLoading: false,
-      drawer : true,
-      bgPopup : false
+      drawer: true,
+      bgPopup: false
     }
   },
 
-  created(){
+  created () {
     this.$EventBus.$on('toggleDrawer', (drawer) => {
-      this.drawer = drawer;
-    });
-    this.$EventBus.$on('main',this.goMain)
-    this.$EventBus.$on('back',this.goBack)
-    this.$EventBus.$on('close', this.close);
-    this.$EventBus.$on('next', this.exportPNG);
-    this.$EventBus.$on('free', (title)=> {
+      this.drawer = drawer
+    })
+    this.$EventBus.$on('main', this.goMain)
+    this.$EventBus.$on('back', this.goBack)
+    this.$EventBus.$on('close', this.close)
+    this.$EventBus.$on('next', this.exportPNG)
+    this.$EventBus.$on('free', (title) => {
       this.freeTitle = title
       this.exportPNG()
-    });
-    this.$EventBus.$on('bgPopup',this.toggleBg);
-    this.$EventBus.$on('freeName',this.setFreeName);
-    if(this.page==='letter'){
+    })
+    this.$EventBus.$on('bgPopup', this.toggleBg)
+    this.$EventBus.$on('freeName', this.setFreeName)
+    if (this.page === 'letter') {
       this.bgPopup = true
     }
   },
   mounted () {
-    if (localStorage.getItem('isReload') === 'true' || localStorage.getItem('isReload') === undefined) window.location.reload()
-    else this.isLoading = true
+    if (localStorage.getItem('isReload') === 'true' || localStorage.getItem('isReload') === undefined) {
+      window.location.reload()
+    } else {
+      this.isLoading = true
 
-    ;(async () => {
-      if(this.page ==='diagnose') await this.fetchSubject()
-      else if(this.page === 'letter') await this.fetchLetter()
+
+    }
+    (async () => {
+      if (this.page === 'diagnose') {
+        await this.fetchSubject()
+      } else if (this.page === 'letter') await this.fetchLetter()
     })()
 
-    if(this.page === '') this.$EventBus.$emit('showToolBar');
-    console.log('page',typeof this.page)
+    if (this.page === '') this.$EventBus.$emit('showToolBar')
+    console.log('page', typeof this.page)
 
-    setTimeout(function () {
-      WILL.setBackground('paper_01')
-      console.log('WILL.setBackground')
-    },3000)
+
+    if (this.page === '' || this.page === 'diagnose') {
+      setTimeout(function () {
+        WILL.setBackground('paper_01')
+        console.log('WILL.setBackground')
+      }, 2000)
+    }
   },
   computed: {
     ...mapGetters({
       session: 'getSession',
       canvasTimer: 'getCanvasTimer',
-      subject : 'getSubject',
+      subject: 'getSubject',
       letter: 'getLetter',
-      bg : 'getBg',
-      canvasList : 'getLetterCanvasList'
+      bg: 'getBg',
+      canvasList: 'getLetterCanvasList'
     }),
-    page() {
+    page () {
       return this.$router.currentRoute.query.page || ''
     }
   },
-  watch:{
-    'canvasTimer.timeOver':function(){
-      if(this.canvasTimer.timeOver===true)
-        this.$refs.timerConfirm.showConfirm=true
-      this.$refs.timerConfirm.type='timeOut'
+  watch: {
+    'canvasTimer.timeOver': function () {
+      if (this.canvasTimer.timeOver === true) {
+        this.$refs.timerConfirm.showConfirm = true
+      }
+      this.$refs.timerConfirm.type = 'timeOut'
     },
-    'isLoading':function(){
-      if(this.isLoading&&this.page==='diagnose'){
-        this.$refs.autoOpen.showAlert=true
-        this.$refs.autoOpen.type='diagnose'
+    'isLoading': function () {
+      if (this.isLoading && this.page === 'diagnose') {
+        this.$refs.autoOpen.showAlert = true
+        this.$refs.autoOpen.type = 'diagnose'
       }
     },
   },
@@ -136,46 +146,48 @@ export default {
     }),
     ...mapActions({
       getSubject: 'getSubject',
-      getLetter : 'getLetter',
-      getSubmissionLearning : 'getSubmissionLearning',
-      getSubmissionFree : 'getSubmissionFree',
-      getSubmission : 'getSubmission'
+      getLetter: 'getLetter',
+      getSubmissionLearning: 'getSubmissionLearning',
+      getSubmissionFree: 'getSubmissionFree',
+      getSubmission: 'getSubmission'
     }),
 
-    reload(){
+    reload () {
       window.location.reload()
     },
-    goNext() {
+    goNext () {
       this.setTimerReset()
       if (this.page === 'diagnose') {
         this.$router.push('/TestingResult')
-      }
-      else if (this.page === 'letter') {
+      } else if (this.page === 'letter') {
         this.$router.push('/Completion')
       }
     },
-    goBack(){
+    goBack () {
       this.$router.push('/Listening')
     },
-    goMain(){
+    goMain () {
       this.$router.push('/PabloMain')
     },
-    close(){
+    close () {
       this.setTimerReset()
       this.$router.push('/')
     },
-    popUpOpen(){
+    popUpOpen () {
       this.setTimerPause()
     },
-    clear(){
+    clear () {
       WILL.clear()
       this.$bvModal.hide('clearAllPopup')
       this.setTimerReset()
     },
-    exportPNG(e){
-      const self = this;
-      WILL.getImageCanvas().toBlob(function(blob) {
-        let file = new File([blob], "my_image.png",{type:"image/png", lastModified:new Date()})
+    exportPNG (e) {
+      const self = this
+      WILL.getImageCanvas().toBlob(function (blob) {
+        let file = new File([blob], 'my_image.png', {
+          type: 'image/png',
+          lastModified: new Date()
+        })
 
         // self.saveFile(URL.createObjectURL(blob))
         //미완성
@@ -193,7 +205,7 @@ export default {
           //학습교제
         } else if (self.page === 'letter') {
           const data = new FormData()
-          data.append('stepId', self.letter.stepId )
+          data.append('stepId', self.letter.stepId)
           data.append('stepPicture', blob, 'canvas.png')
           data.append('imageId', self.bg.imageId)
           self.fetchSubmissionLearning(data) //학습 정보 드로잉 제출 API
@@ -206,46 +218,46 @@ export default {
         }
       })
     },
-    saveFile(href){
-      var a = document.createElement("a");
-      a.href = href;
-      a.download = 'down.png';
+    saveFile (href) {
+      var a = document.createElement('a')
+      a.href = href
+      a.download = 'down.png'
 
-      a.appendChild(document.createTextNode('down.png'));
-      a.style.display = "none";
+      a.appendChild(document.createTextNode('down.png'))
+      a.style.display = 'none'
 
-      document.body.appendChild(a);
-      a.click();
+      document.body.appendChild(a)
+      a.click()
 
-      setTimeout(function() {
-        URL.revokeObjectURL(href);
-      }, 911);
+      setTimeout(function () {
+        URL.revokeObjectURL(href)
+      }, 911)
     },
-    toggleBg(){
-      this.bgPopup = !this.bgPopup;
-      if(this.bgPopup===false&&this.page==='diagnose'){
-        this.setTimerStart();
-      }else if(this.page==='letter'){
-        this.bgPopup=false
+    toggleBg () {
+      this.bgPopup = !this.bgPopup
+      if (this.bgPopup === false && this.page === 'diagnose') {
+        this.setTimerStart()
+      } else if (this.page === 'letter') {
+        this.bgPopup = false
         this.setBackgrounImage()
-      } else if(this.page===''){
+      } else if (this.page === '') {
         WILL.setBackground('paper_01')
       }
     },
-    toggleGuide(){
-      this.bgPopup = !this.bgPopup;
+    toggleGuide () {
+      this.bgPopup = !this.bgPopup
 
-      if(this.bgPopup===false&&this.page==='diagnose'){
-        this.setTimerStart();
-      }else if(this.page==='letter'){
-        this.bgPopup=false
+      if (this.bgPopup === false && this.page === 'diagnose') {
+        this.setTimerStart()
+      } else if (this.page === 'letter') {
+        this.bgPopup = false
         this.setBackgrounImage()
       }
-      this.$EventBus.$emit('showToolBar');
+      this.$EventBus.$emit('showToolBar')
     },
 
-    setFreeName(){
-      this.$refs.register.showRegister=true
+    setFreeName () {
+      this.$refs.register.showRegister = true
     },
     async fetchSubject () {
       this.getSubject()
@@ -253,28 +265,30 @@ export default {
           this.setTimeInit(this.subject.limitTime)
         })
     },
-    async fetchLetter(){
+    async fetchLetter () {
       this.getLetter()
     },
-    fetchSubmission(data){
-      const self = this;
+    fetchSubmission (data) {
+      const self = this
       this.getSubmission(data)
         .then(result => {
-          if(result.code === '0000') {
+          if (result.code === '0000') {
             this.$refs.autoOpenSuccess.showAlert = true
             this.$refs.autoOpenSuccess.type = 'success'
-          } else alert(`code : ${result.code} message : ${result.message}`)
+          } else {
+            alert(`code : ${result.code} message : ${result.message}`)
+          }
         })
     },
-    fetchSubmissionLearning(data){
-      const self = this;
+    fetchSubmissionLearning (data) {
+      const self = this
       this.getSubmissionLearning(data)
         .then(result => {
-          if(result.code === '0000') {
-            if(this.canvasList.length === 1) {
+          if (result.code === '0000') {
+            if (this.canvasList.length === 1) {
               this.getLetter()
-                .then( result => {
-                  this.setBg({reset : true})
+                .then(result => {
+                  this.setBg({ reset: true })
                   this.$router.push('/Completion')
                 })
             } else {
@@ -288,12 +302,12 @@ export default {
           }
         })
     },
-    fetchSubmissionFree(data){
-      const self = this;
+    fetchSubmissionFree (data) {
+      const self = this
       this.getSubmissionFree(data)
         .then(result => {
-          if(result.code === '0000') {
-            self.$router.push('/Recording?page=free&freeTitle='+this.freeTitle)
+          if (result.code === '0000') {
+            self.$router.push('/Recording?page=free&freeTitle=' + this.freeTitle)
           } else {
             // alert('드로잉 제출 실패')
             self.$refs.submissionFail.showAlert = true
@@ -301,13 +315,13 @@ export default {
           }
         })
     },
-    setBackgrounImage(){
+    setBackgrounImage () {
       // WILL.setBackground(this.canvasList[0].tabletImageUrl, 'url')
       // this.bg.imageId = this.canvasList[0].imageId
       // this.bg.imageName = this.canvasList[0].imageName
 
       WILL.clear()
-      this.$EventBus.$emit('setBg', this.canvasList[0] , false)
+      this.$EventBus.$emit('setBg', this.canvasList[0], false)
       this.Android.tts(this.canvasList[0].imageSubject)
     }
   }
@@ -323,12 +337,14 @@ export default {
   top: 0;
   left: 0;
   z-index: 1111;
+
   img {
     width: 100%;
     height: 100%;
   }
 }
-.guide_bg{
+
+.guide_bg {
   position: absolute;
   top: 0;
   left: 0;
